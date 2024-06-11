7514f07bf152850e1384dd6b3202f346479ab430
Albert <morkeylukel@gmail.com>
2024-06-11 13:06:38 +0800

    0611

---
+++ 0611/Es6/mathUtil.js
@@ -0,0 +1,13 @@
+export const PI = 3.14159;
+
+export function getCircumreference(radius) {
+  return PI * 2 * radius;
+}
+
+export function getArea(radius) {
+  return PI * raius * radius;
+}
+
+export function getVoluem(params) {
+  return (4 / 3) * PI * radius * radius * radius;
+}
